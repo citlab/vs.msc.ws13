8152007f877ab55cf49f68b0556f5a84c6471c91
Kay Fleischmann <kay.fleischmann@gmx.de>
2014-01-19 17:47:00 +0100

    MergJoin Operator added

--- master/src/main/java/de/tu_berlin/citlab/storm/operators/join/MergeJoin.java
+++ master/src/main/java/de/tu_berlin/citlab/storm/operators/join/MergeJoin.java
@@ -10,30 +10,44 @@ import backtype.storm.tuple.Tuple;
 public class MergeJoin implements JoinUDF {
 	private static final long serialVersionUID = -318981036829194511L;
 
-	public void executeJoin(JoinPair pair, TupleComparator comparator, TupleProjection projection, OutputCollector collector ) {
+	public void executeJoin(JoinPair pair, TupleComparator tupleComparator, TupleProjection projection, OutputCollector collector ) {
 		
 		Iterator<Tuple> outerTuples = pair.getOuter().getWindow().iterator();
 		Iterator<Tuple> innerTuples = pair.getInner().getWindow().iterator();
 		LinkedList<Tuple> activeList = new LinkedList<Tuple>(); 
 
-		Tuple currentLeftTuple = outerTuples.next();
-		Tuple currentRightTuple = innerTuples.next();
+		Tuple currentOuterTuple = outerTuples.next();
+		Tuple currentInnerTuple = innerTuples.next();
 
 		while (outerTuples.hasNext() && innerTuples.hasNext()) {
 
-			/*
-			if(currentLeftTuple)
-			
-			while (innerTuples.hasNext()) {
-				Tuple innerTuple = outerTuples.next();
+			if( tupleComparator.compare( currentOuterTuple, currentInnerTuple) < 0  ){
+				outerTuples.next();
+			}
+			else if( tupleComparator.compare( currentOuterTuple, currentInnerTuple) > 0 ){
+				innerTuples.next();
+			}
+			else {	
+				/* keys are the same */
+				activeList.clear();
+				Tuple keyTuple = currentOuterTuple;
 				
-				// find matches
-				/*if( predicate.evaluate( outerTuple,innerTuple ) ) {
-					
-				}//while
+				// find matching tuples from inner and store the matched list separately
+				while(innerTuples.hasNext() && tupleComparator.compare( keyTuple, currentInnerTuple) == 0 ){
+					activeList.add( currentInnerTuple );
+					currentInnerTuple = innerTuples.next();
+				}
+				
+				// for each matching on the outer side emit a tuple with all matched inner side
+				while(outerTuples.hasNext() && tupleComparator.compare( currentOuterTuple, keyTuple) == 0 ){
+					for( Tuple e : activeList ){
+						collector.emit(	projection.project(currentOuterTuple,e)	);
+					}//for
+					currentOuterTuple = outerTuples.next();
+				} //while
+			} //if
 			
-			}//while	
-			*/
-		}
+		} // while
+		
 	}
 }
--- master/src/main/java/de/tu_berlin/citlab/storm/window/WindowContainer.java
+++ master/src/main/java/de/tu_berlin/citlab/storm/window/WindowContainer.java
@@ -8,7 +8,6 @@ public class WindowContainer<I> implements Serializable, Cloneable
 	private static final long serialVersionUID = 1L;
 		
 	private final List<I> window;
-
 	
 	public WindowContainer(List<I> w){
 		this.window = w;
@@ -16,5 +15,9 @@ public class WindowContainer<I> implements Serializable, Cloneable
 	
 	public List<I> getWindow(){
 		return window;
+	}
+	
+	public void sort(  ){
+		
 	}
 }
