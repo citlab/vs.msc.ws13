4703ef839942e21e62e6fa09c940a271d034557f
Kay Fleischmann <kay.fleischmann@gmx.de>
2014-02-27 11:49:05 +0100

    cleanup

---
+++ master/src/main/java/de/tu_berlin/citlab/storm/examples/SlidingCountWindowMergeJoinTestTopologyTwoSources.java
@@ -0,0 +1,118 @@
+package de.tu_berlin.citlab.storm.examples;
+
+
+import java.util.Map;
+
+import backtype.storm.Config;
+import backtype.storm.LocalCluster;
+import backtype.storm.spout.SpoutOutputCollector;
+import backtype.storm.task.TopologyContext;
+import backtype.storm.topology.OutputFieldsDeclarer;
+import backtype.storm.topology.TopologyBuilder;
+import backtype.storm.topology.base.BaseRichSpout;
+import backtype.storm.tuple.Fields;
+import backtype.storm.tuple.Tuple;
+import backtype.storm.tuple.Values;
+import backtype.storm.utils.Utils;
+import de.tu_berlin.citlab.storm.bolts.UDFBolt;
+import de.tu_berlin.citlab.storm.helpers.KeyConfigFactory;
+import de.tu_berlin.citlab.storm.operators.join.JoinOperator;
+import de.tu_berlin.citlab.storm.operators.join.SimpleHashJoin;
+import de.tu_berlin.citlab.storm.operators.join.TupleProjection;
+import de.tu_berlin.citlab.storm.window.CountWindow;
+
+public class SlidingCountWindowMergeJoinTestTopologyTwoSources {
+	
+	private static final int windowSize = 10;
+	private static final int slidingOffset = 10;
+	
+	@SuppressWarnings("serial")
+	public static void main(String[] args) throws Exception {
+		
+		class DataSource extends BaseRichSpout {
+			
+			private static final long serialVersionUID = -7374814904789368773L;
+			
+			private String[] ids = new String[] { "key1", "key2" , "key3" };
+			
+			private int currentId = 0;
+
+			int _id = 0;
+
+			SpoutOutputCollector _collector;
+
+			@Override
+			public void ack(Object msgId) {
+			}
+
+			@Override
+			public void fail(Object msgId) {
+			}
+
+			public void open(@SuppressWarnings("rawtypes") Map conf,
+					TopologyContext context, SpoutOutputCollector collector) {
+				_collector = collector;
+			}
+
+			public void nextTuple() {
+				Utils.sleep(500);
+				_collector.emit(new Values(ids[currentId++ % ids.length], _id));
+				_id++;
+			}
+
+			public void declareOutputFields(OutputFieldsDeclarer declarer) {
+				declarer.declare(new Fields("key", "value"));
+			}
+
+			@Override
+			public Map<String, Object> getComponentConfiguration() {
+				return null;
+			}
+		}		
+
+		
+		
+		TopologyBuilder builder = new TopologyBuilder();
+		builder.setSpout("s1", new DataSource(), 1);
+		builder.setSpout("s2", new DataSource(), 1);
+		
+		
+		TupleProjection projection = new TupleProjection(){
+			public Values project(Tuple left, Tuple right) {
+				
+				return new Values(  left.getValueByField("key"),
+									left.getValueByField("value"),
+									right.getValueByField("key"),
+									right.getValueByField("value")
+									
+								);
+			}
+		};
+		
+		
+		builder.setBolt("slide",
+			new UDFBolt(
+				null, // no outputFields
+				new JoinOperator( 
+								new SimpleHashJoin(), 
+								KeyConfigFactory.compareByFields(new Fields("key")), 
+								projection, 
+								"s1", "s2" ), 
+				new CountWindow<Tuple>(windowSize, slidingOffset),
+				KeyConfigFactory.BySource()
+			),
+		1)	.shuffleGrouping("s1")
+			.shuffleGrouping("s2");
+
+		
+		Config conf = new Config();
+		conf.setDebug(true);
+
+		conf.setMaxTaskParallelism(1);
+		conf.setMaxSpoutPending(1);
+
+		LocalCluster cluster = new LocalCluster();
+		cluster.submitTopology("sliding-count-window-group-by-test", conf,
+				builder.createTopology());
+	}
+}
---
+++ master/src/main/java/de/tu_berlin/citlab/storm/examples/UDFTestTopology.java
@@ -0,0 +1,120 @@
+package de.tu_berlin.citlab.storm.examples;
+
+import java.util.List;
+
+import backtype.storm.Config;
+import backtype.storm.LocalCluster;
+import backtype.storm.topology.TopologyBuilder;
+import backtype.storm.tuple.Fields;
+import backtype.storm.tuple.Tuple;
+import backtype.storm.tuple.Values;
+import de.tu_berlin.citlab.storm.bolts.UDFBolt;
+import de.tu_berlin.citlab.storm.operators.FilterOperator;
+import de.tu_berlin.citlab.storm.operators.FilterUDF;
+import de.tu_berlin.citlab.storm.udf.IOperator;
+import de.tu_berlin.citlab.storm.window.CountWindow;
+import backtype.storm.task.OutputCollector;
+public class UDFTestTopology {
+
+	@SuppressWarnings("serial")
+	public static void main(String[] args) throws Exception {
+
+		TopologyBuilder builder = new TopologyBuilder();
+
+		builder.setSpout("spout", new CounterProducer(), 1);
+		builder.setBolt(
+			"map",
+			new UDFBolt(
+				new Fields("key", "value"),
+				new IOperator() {
+					public void execute(List<Tuple> param, OutputCollector collector ) {
+						String newKey = param.get(0).getStringByField("key") + " mapped";
+						int newValue = myExistingFunction((Integer) param
+								.get(0).getValues().get(1));
+
+						collector.emit( new Values(newKey, newValue));
+
+					}
+					public int myExistingFunction(int param) {
+						return param + 10;
+					}
+				}
+			),
+			1
+		).shuffleGrouping("spout");
+		
+		
+		builder.setBolt(
+			"flatmap",
+			new UDFBolt(
+				new Fields("key", "value"),
+				new IOperator() {
+					public void execute(List<Tuple> param, OutputCollector collector ) {
+						String inputKey = (String) param.get(0).getValues().get(0);
+						int inputValue = (Integer) param.get(0).getValues().get(1);
+						collector.emit( new Values(inputKey + "flatmapped1",
+								myExistingFunction1(inputValue)) ) ;
+						
+						collector.emit( new Values(inputKey + "flatmapped2",
+								myExistingFunction2(inputValue)));
+					}
+
+					private int myExistingFunction1(int param) {
+						return param *= 10;
+					}
+
+					private int myExistingFunction2(int param) {
+						return param *= -1;
+					}
+				}
+			),
+		1).shuffleGrouping("map");
+		
+		
+		builder.setBolt(
+			"filter",
+			new UDFBolt(
+				new Fields("value"), // output
+				new FilterOperator(
+					new Fields("value"), // input
+					new FilterUDF() {
+						public Boolean evaluate(Tuple param) {
+							return (Integer) param.getValueByField("value") > 0;
+						}
+					}
+				)
+			),
+			1
+		).shuffleGrouping("flatmap");
+		
+		
+		builder.setBolt(
+			"reducer",
+			new UDFBolt(
+				new Fields("value"),
+				new IOperator() {
+					public void execute(List<Tuple> param, OutputCollector collector ) {
+						int reduced = 0;
+						for (Tuple tupel : param) {
+							reduced += (Integer) tupel.getValueByField("value");
+						}
+						collector.emit( new Values(reduced) );
+					}
+				},
+				new CountWindow<Tuple>(2)
+			),
+			1
+		).fieldsGrouping("filter", new Fields("value"));
+
+		
+		Config conf = new Config();
+		conf.setDebug(true);
+
+		conf.setMaxTaskParallelism(1);
+		conf.setMaxSpoutPending(1);
+
+		LocalCluster cluster = new LocalCluster();
+		cluster.submitTopology("overflow-test", conf, builder.createTopology());
+	}
+
+}
