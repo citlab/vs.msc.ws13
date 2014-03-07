07f2201ad5c3f0d8af231fb42114342574ce63b0
Kay Fleischmann <kay.fleischmann@gmx.de>
2014-03-07 15:08:58 +0100

    analyze topology

---
+++ master/src/main/java/de/tu_berlin/citlab/storm/topologies/AnalyzeTweetsTopology.java
@@ -0,0 +1,15 @@
+package de.tu_berlin.citlab.storm.topologies;
+
+
+
+public class AnalyzeTweetsTopology {
+    private static final int windowSize = 1000;
+    private static final int slidingOffset = 500;
+
+
+    @SuppressWarnings("serial")
+    public static void main(String[] args) throws Exception {
+
+
+    }
+}
