e51d7f1da1bded75f1091959b298942f63437d18
Kay Fleischmann <kay.fleischmann@gmx.de>
2014-03-11 16:41:39 +0100

    topology with updating user total-significance

--- master/src/main/java/de/tu_berlin/citlab/storm/topologies/FilterBadUserMultipleOperators.java
+++ master/src/main/java/de/tu_berlin/citlab/storm/topologies/FilterBadUserMultipleOperators.java
@@ -12,4 +12,24 @@ public class FilterBadUserMultipleOperators extends MultipleOperators implements
         super(operators);
     }
     public static final Map<String,Integer> badUsers = new HashMap<String,Integer>();
+    public static boolean isDetectedUser(String user){
+        if( FilterBadUserMultipleOperators.badUsers.containsKey(user) ){
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public static void updateDetectedUser(String user, int total_significance ){
+        if( isDetectedUser(user) ){
+            Integer new_significance = FilterBadUserMultipleOperators.badUsers.get(user) + total_significance;
+            FilterBadUserMultipleOperators.badUsers.put(user, new_significance);
+
+            System.out.println("new detected user: " + user + ", totalsignificance: " + new_significance );
+
+        } else {
+            FilterBadUserMultipleOperators.badUsers.put(user, total_significance);
+
+            System.out.println("new detected user: " + user + ", totalsignificance: " + total_significance);
+        }
+    }
 }
