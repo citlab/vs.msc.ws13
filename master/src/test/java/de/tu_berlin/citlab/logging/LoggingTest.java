74ad68ec5b5a7af4b498db7f2269cd6e286952a3
Fridtjof <fsander@mailbox.tu-berlin.de>
2014-03-18 12:11:59 +0100

    added tests for new logging

--- master/src/test/java/de/tu_berlin/citlab/logging/LoggingTest.java
+++ master/src/test/java/de/tu_berlin/citlab/logging/LoggingTest.java
@@ -1,22 +1,25 @@
 package de.tu_berlin.citlab.logging;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MarkerFactory;
 
 public class LoggingTest {
 	
-	
-	private static Logger log = LogManager.getLogger(LoggingTest.class);
+	private static Logger log = LoggerFactory.getLogger(LoggingTest.class);
 
-	public static void main(String[] args) throws InstantiationException, IllegalAccessException, ClassNotFoundException {
+	@SuppressWarnings("serial")
+	public static void main(String[] args) {
+		// log levels
 		log.trace("trace");
 		log.debug("debug");
 		log.info("info");
 		log.warn("warn");
 		log.error("error");
-		log.fatal("fatal");
-		
+		// exceptions
+		log.error("kaputt", new RuntimeException("panic!!!"));
+		// marker
+		log.info(MarkerFactory.getMarker("marker1"), "this is a marked message");
 	}
 }
