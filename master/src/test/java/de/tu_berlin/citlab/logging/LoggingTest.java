94de4f5a00003f14fd0ac76b8b6e162639658939
Fridtjof <fsander@mailbox.tu-berlin.de>
2014-03-18 15:39:39 +0100

    fixed logging redirects for log4j 1.2 and slf4j

--- master/src/test/java/de/tu_berlin/citlab/logging/LoggingTest.java
+++ master/src/test/java/de/tu_berlin/citlab/logging/LoggingTest.java
@@ -1,16 +1,18 @@
 package de.tu_berlin.citlab.logging;
 
-
 import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.slf4j.MarkerFactory;
+
 
 public class LoggingTest {
-	
-	private static Logger log = LoggerFactory.getLogger(LoggingTest.class);
 
-	@SuppressWarnings("serial")
 	public static void main(String[] args) {
+		SLF4JLoggerTest();
+		Log4J12LoggerTest();
+		Log4J2LoggerTest();
+	}
+
+	private static void SLF4JLoggerTest() {
+		org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(LoggingTest.class + "->slf4j");
 		// log levels
 		log.trace("trace");
 		log.debug("debug");
@@ -20,6 +22,36 @@ public class LoggingTest {
 		// exceptions
 		log.error("kaputt", new RuntimeException("panic!!!"));
 		// marker
-		log.info(MarkerFactory.getMarker("marker1"), "this is a marked message");
+		log.info(org.slf4j.MarkerFactory.getMarker("marker1"), "this is a marked message");
 	}
+	
+	
+	private static void Log4J12LoggerTest() {
+		org.apache.log4j.Logger log = org.apache.log4j.LogManager.getLogger(LoggingTest.class.getName() + "->log4j1.2");
+		// log levels
+		log.trace("trace");
+		log.debug("debug");
+		log.info("info");
+		log.warn("warn");
+		log.error("error");
+		// exceptions
+		log.error("kaputt", new RuntimeException("panic!!!"));
+		// marker
+		// not existent in log4j 1.2
+	}
+	
+	private static void Log4J2LoggerTest() {
+		org.apache.logging.log4j.Logger log = org.apache.logging.log4j.LogManager.getLogger(LoggingTest.class.getName() + "->log4j2");
+		// log levels
+		log.trace("trace");
+		log.debug("asd");
+		log.info("info");
+		log.warn("warn");
+		log.error("error");
+		// exceptions
+		log.error("kaputt", new RuntimeException("panic!!!"));
+		// marker
+		log.info(org.apache.logging.log4j.MarkerManager.getMarker("marker1"), "this is a marked message");
+	}
+
 }
