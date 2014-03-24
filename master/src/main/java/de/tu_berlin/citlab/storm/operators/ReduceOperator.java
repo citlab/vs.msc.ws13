a649d8e624d444ab095a92973a8730d3dbc60f9d
Kay Fleischmann <kay.fleischmann@gmx.de>
2014-03-24 14:49:43 +0100

    reduce operator refactored allow simple types

--- master/src/main/java/de/tu_berlin/citlab/storm/operators/ReduceOperator.java
+++ master/src/main/java/de/tu_berlin/citlab/storm/operators/ReduceOperator.java
@@ -3,36 +3,37 @@ package de.tu_berlin.citlab.storm.operators;
 import java.util.ArrayList;
 import java.util.List;
 
+import backtype.storm.tuple.Values;
 import de.tu_berlin.citlab.storm.udf.IOperator;
 import backtype.storm.task.OutputCollector;
 import backtype.storm.tuple.Tuple;
 
+
 @SuppressWarnings("serial")
 public class ReduceOperator<T> extends IOperator {
-	
-	protected final Reducer<T> reducer;
-	protected final T init;
-	protected boolean chaining = false;
 
-	public ReduceOperator(Reducer<T> reducer, T init) {
-		this.reducer = reducer;
-		this.init = init;
-	}
+    protected final Reducer<T> reducer;
+    protected final T init;
+    protected boolean chaining = false;
 
-	public ReduceOperator setChainingAndReturnInstance(boolean chaining) {
-		this.chaining = chaining;
-		return this;
-	}
+    public ReduceOperator(Reducer<T> reducer, T init) {
+        this.reducer = reducer;
+        this.init = init;
+    }
 
-	public void execute(List<Tuple> input, OutputCollector emitter) {
-		T result = init;
-		for (Tuple t : input) {
-			result = reducer.reduce(result, t);
-		}
-		if (chaining) {
-			emitter.emit(input, result);
-		} else {
-			emitter.emit(result);
-		}
-	}
+    public void execute(List<Tuple> input, OutputCollector emitter) {
+        T result = init;
+        for (Tuple t : input) {
+            result = reducer.reduce(result, t);
+        }
+        if (chaining) {
+            emitter.emit(input, envelope(input, result));
+        } else {
+            emitter.emit(envelope(input, result));
+        }
+    }
+
+    public Values envelope(List<Tuple> input, T result){
+        return new Values(result);
+    }
 }
