35381fc9d23c79fe53771bbdbb564b2104b6208c
Gert Geidel <gertimon@Gerts-MacBook-Pro.local>
2014-03-24 14:24:56 +0100

    List Users and show status

--- webinterface/app/models/User.java
+++ webinterface/app/models/User.java
@@ -7,6 +7,29 @@ import play.mvc.Http.*;
 import models.Database;
 
 public class User {
+
+  private String name;
+  private int id;
+  private String session;
+
+  public User(String name, int id, String session) {
+    this.name = name;
+    this.id = id;
+    this.session = session;
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public int getId() {
+    return this.id;
+  }
+
+  public String getStatus() {
+    return session != null ? "online" : "offline";
+  }
+
   public static boolean loggedIn(boolean onlyLoggedIn, String name, String sessionId) {
     if (!onlyLoggedIn) return true;
     return name!=null && Database.getInstance().checkSession(name, sessionId);
@@ -15,4 +38,6 @@ public class User {
   public static boolean isAdmin(String name) {
     return name.equals("admin");
   }
+
+
 }
