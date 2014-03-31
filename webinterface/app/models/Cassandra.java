8b55b2196a45381051ef3a66c8e4d4254738544d
Gert Geidel <gertimon@wlan-141-23-70-70.tubit.tu-berlin.de>
2014-03-31 10:36:51 +0200

    Filter begin

--- webinterface/app/models/Cassandra.java
+++ webinterface/app/models/Cassandra.java
@@ -1,5 +1,8 @@
 package models;
 
+import models.aws.Instance;
+import models.aws.AwsCli;
+
 import com.github.kevinsawicki.http.*;
 
 public class Cassandra {
@@ -11,5 +14,21 @@ public class Cassandra {
 
   public static Boolean isUp() {
     return Cassandra.getIp() != null;
+  }
+
+  public static String getStatus() {
+    try {
+      Instance[] instances = AwsCli.describeInstances();
+      Instance instance = null;
+      String imageId = ClusterDatabase.getInstance().getProperty("cassandra.image-id");
+
+      for(Instance inst : instances) {
+        if(inst.getImageId().equals(imageId)) instance = inst;      
+      }
+
+      return instance == null ? "Stopped" : instance.toString();
+    } catch(Exception e) {
+      return "Local";
+    }
   }
 }
--- webinterface/app/models/Nimbus.java
+++ webinterface/app/models/Nimbus.java
@@ -1,5 +1,8 @@
 package models;
 
+import models.aws.Instance;
+import models.aws.AwsCli;
+
 import com.github.kevinsawicki.http.*;
 
 public class Nimbus {
@@ -11,5 +14,21 @@ public class Nimbus {
 
   public static Boolean isUp() {
     return Nimbus.getIp() != null;
+  }
+
+  public static String getStatus() {
+    try {
+      Instance[] instances = AwsCli.describeInstances();
+      Instance instance = null;
+      String imageId = ClusterDatabase.getInstance().getProperty("nimbus.image-id");
+
+      for(Instance inst : instances) {
+        if(inst.getImageId().equals(imageId)) instance = inst;      
+      }
+
+      return instance == null ? "Stopped" : instance.toString();
+    } catch(Exception e) {
+      return "Local";
+    }
   }
 }
